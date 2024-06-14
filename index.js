0a3c0a95aa43c44ce881a8d3ba3da37e470e9543
mane-23G <mane.galstyan87@myhunter.cuny.edu>
2024-06-14 17:26:39 -0400

    The foundation
    updated the organization of folders, and set up the navbar

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
+let app = express();
+let port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+// app.set('view engine', 'ejs');
+
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+});
+
+app.get("/theaters", (req,res) => {
+    res.render("theaters.ejs");
+});
+
+app.get("/genres", (req,res) => {
+    res.render("genres.ejs");
+});
+
+app.get("/events", (req,res) => {
+    res.render("events.ejs");
+});
+
+app.get("/about", (req,res) => {
+    res.render("about.ejs");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
