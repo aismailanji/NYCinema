07e7e1e267abdbabf80c13d732b532a0ef5ecbfd
mane-23G <mane.galstyan87@myhunter.cuny.edu>
2024-07-07 16:42:47 -0400

    more work done for the log in and sign up
    the log in and register work but they need to be further implemented using access tokens so the log in session actually works

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ import axios from "axios";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import * as cheerio from 'cheerio';
 
 let app = express();
 let port = 3000;
 const saltRounds = 10;
 
 // this is for the database will uncomment once the sharing ability is figured out
-// const db = new pg.Client({
-//     user: "postgres",
-//     host: "localhost",
-//     database: "nycinema",
-//     password: "Hello123",
-//     port: 5433,
-// });
-// db.connect();
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "nycinema",
+    password: "",
+    port: 5433,
+});
+db.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -44,42 +45,78 @@ app.get("/about", (req,res) => {
     res.render("about.ejs");
 });
 
-// app.post("/register", async (req,res) => {
-//     const username = req.body.username;
-//     const email = req.body.email;
-//     const password = req.body.password;
+app.post("/register", async (req,res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
 
 
-//     try {
-//         const checkResult = await db.query("SELECT * FROM accounts WHERE email = $1", [email,]);
+    try {
+        const result = await db.query("SELECT * FROM accounts WHERE email = $1", [email,]);
 
-//         if(checkResult.rows.length > 0) {
-//             res.json({msg: "Email already exists. Try logging in."});
-//         }
-//         else {
-//             console.log("username: ", username);
-//             console.log("email: ", email);
-//             console.log("password: ", password);
-//             bcrypt.hash(password, saltRounds, async (err, hash) => {
-//                 if(err) {
-//                     console.error("Error hashing password: ", err);
-//                     res.json({msg: "Error hashing password."});
-//                 }
-//                 else {
-//                     console.log("Hashed Password: ", hash);
-//                     await db.query(
-//                         "INSERT INTO accounts (username, email, password) VALUES ($1,$2,$3)", [username, email, hash]
-//                     );
-//                     res.json({msg: "Registration complete. Please log in."});
-//                 }
-//             })
-//         }
-//     }
-//     catch(err) {
-//         console.log(err);
-//         res.json({msg: "An error occurred. Please try again."});
-//     }
-// });
+        if(result.rows.length > 0) {
+            res.json({msg: "Email already exists. Try logging in."});
+        }
+        else {
+            bcrypt.hash(password, saltRounds, async (err, hash) => {
+                if(err) {
+                    console.error("Error hashing password: ", err);
+                    res.json({msg: "Error hashing password."});
+                }
+                else {
+                    console.log("Hashed Password: ", hash);
+                    await db.query(
+                        "INSERT INTO accounts (username, email, password) VALUES ($1,$2,$3)", [username, email, hash]
+                    );
+                    res.json({msg: "Registration complete. Please log in."});
+                }
+            })
+        }
+    }
+    catch(err) {
+        console.log(err);
+        res.json({msg: "An error occurred. Please try again."});
+    }
+});
+
+app.post("/login", async (req,res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let accountExists = false;
+    try {
+        const result = await db.query("SELECT * FROM accounts WHERE email = $1", [email,]);
+
+        if(result.rows.length > 0) {
+            const user = result.rows[0];
+            const storedHashedPassword = user.password;
+
+            bcrypt.compare(password,storedHashedPassword, (err,result) => {
+                if(err) {
+                    console.error("Error comparing passwords: ", err);
+                }
+                else {
+                    if(result) {
+                        console.log("Account found and password correct.");
+                        accountExists = true;
+                        // this can be changed depending on what we want the user to see after logging in.
+                    }
+                    else {
+                        let fakePass = `$2b$$10$ifgfgfgfgfgfgfggfgfgfggggfgfgfga`;
+                        bcrypt.compare(req.body.password, fakePass);
+                        res.json({msg: "Invalid email or password."})
+                    }
+                }
+            });
+        }
+        else {
+            res.json({msg: "User not found."})
+        }
+    }
+    catch(err) {
+        console.log(err);
+        res.json({msg: "An error occurred. Please try again."});
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -24,8 +24,8 @@ document.addEventListener("DOMContentLoaded", function() {
         wrap.classList.remove("active-popup");
     });
 
-    const login_sub = document.querySelector('.register-submit')
-    login_sub.addEventListener('submit', async function(event) {
+    const reg_sub = document.querySelector('.register-submit')
+    reg_sub.addEventListener('submit', async function(event) {
         event.preventDefault();
         const form = event.target;
         const formData = new FormData(form);
@@ -44,7 +44,37 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+    const login_sub = document.querySelector('.login-submit')
+    login_sub.addEventListener('submit', async function(event) {
+        event.preventDefault();
+        const form = event.target;
+        const formData = new FormData(form);
+        try {
+            const response = await fetch('/login', {
+                method: 'POST',
+                body: new URLSearchParams(formData)
+            });
+
+            const data = await response.json();
+            if(data.accountExists == true) {
+                wrap.classList.remove("active-popup");
+                document.querySelector('#login').style.width = "110px";
+                document.querySelector('#login').textContent = "My Account";
+            }
+            else {
+            const result = data.msg;
+            document.querySelector('.msg').textContent = result;
+            document.querySelector('.msg').classList.add("active");
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
 
 
-
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+        document.querySelector('#login').style.width = "110px";
+        document.querySelector('#login').textContent = "My Account";
+        wrap.classList.remove("active-popup");
+    }
 });
