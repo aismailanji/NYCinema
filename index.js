a17770efec27819ed6c27253f91ad23d096106f6
mane-23G <mane.galstyan87@myhunter.cuny.edu>
2024-08-17 23:40:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -34,9 +34,7 @@ const db = new pg.Client({
     host: "localhost",
     database: "nycinema",
     password: "hello123",
-    port: 5432,
-    password: "MaTroy23",
-    port: 5433,
+    port: 5432
 });
 // db.connect();
 db.connect(err => {
